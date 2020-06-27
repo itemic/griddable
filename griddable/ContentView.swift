7dbb6b32a9e5b34cfd74ce0975e7ec1d7492004c
Terran Kroft <terrankroft@gmail.com>
2020-06-27 14:07:48 +0800

    Initial Commit

---
+++ griddable/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  griddable
+//
+//  Created by Terran Kroft on 6/27/20.
+//  Copyright © 2020 itemic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
